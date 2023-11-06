f9ff4b4440140ec6733a4c321f797f5429c2b1d8
sheng.wang <justwangsheng@gmail.com>
2023-11-06 13:52:48 +0800

    fix(resty): fix ParseResp and add AsRespErr

--- resty/response.go
+++ resty/response.go
@@ -1,29 +1,49 @@
 package resty
 
 import (
+	"errors"
 	"fmt"
 )
 
-type respErr interface {
+type iRespErr interface {
 	String() string
 }
 
-type respRes interface {
+type iRespRes interface {
 	IsSuccess() bool
 	String() string
 }
 
-func ParseResp[Err respErr, Res respRes](resp *Response, ee error) (t Res, _ error) {
+func ParseResp[Err iRespErr, Res iRespRes](resp *Response, ee error) (t Res, _ error) {
 	if ee != nil {
-		return t, fmt.Errorf("execute error: %w", ee)
+		return t, &RespErr{Response: resp, err: fmt.Errorf("execute error: %w", ee)}
 	}
 	if resp.IsError() {
 		err := resp.Error().(Err)
-		return t, fmt.Errorf("response status: %s error: %s", resp.Status(), err.String())
+		return t, &RespErr{Response: resp, msg: err.String()}
 	}
 	res := resp.Result().(Res)
 	if !res.IsSuccess() {
-		return t, fmt.Errorf("response status: %s error: %s", resp.Status(), res.String())
+		return t, &RespErr{Response: resp, msg: res.String()}
 	}
 	return res, nil
+}
+
+type RespErr struct {
+	*Response
+	err error
+	msg string
+}
+
+func (e *RespErr) Error() string {
+	if e.err != nil {
+		e.msg = e.err.Error()
+	}
+	return fmt.Sprintf("response status: %s, error: %s", e.Response.Status(), e.msg)
+}
+
+func AsRespErr(err error) (*RespErr, bool) {
+	re := &RespErr{}
+	ok := errors.As(err, &re)
+	return re, ok
 }
