548bab2ae5c60d572639bb27f3ed0805260d74a1
sheng.wang <justwangsheng@gmail.com>
2023-11-07 14:24:07 +0800

    fix(utils): fix caller func return entry

--- core/utils/osutil/caller.go
+++ core/utils/osutil/caller.go
@@ -71,7 +71,7 @@ func CallerFn(fn func(frame CallerFrame) bool, skip ...int) uintptr {
 	for {
 		frame, more := frames.Next()
 		if !fn(frame) {
-			return frame.PC
+			return frame.Entry
 		}
 		if !more {
 			break
