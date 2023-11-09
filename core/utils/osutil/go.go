bbbf7cf7e5b30b6f2d42c771e2a58786d3042a13
sheng.wang <justwangsheng@gmail.com>
2023-11-09 09:22:37 +0800

    perf(utils): perfect go root path

--- core/utils/osutil/go.go
+++ core/utils/osutil/go.go
@@ -3,18 +3,43 @@ package osutil
 import (
 	"encoding/json"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
-func GoListRoot() string {
-	output, err := exec.Command("go", "list", "-json").Output()
-	if err == nil {
-		var res = struct {
-			Root string `json:"Root"`
-		}{}
-		err = json.Unmarshal(output, &res)
+type GoList struct {
+	Dir        string `json:"Dir"`
+	ImportPath string `json:"ImportPath"`
+	Name       string `json:"Name"`
+	Root       string `json:"Root"`
+	Module     struct {
+		Path      string `json:"Path"`
+		Main      bool   `json:"Main"`
+		Dir       string `json:"Dir"`
+		GoMod     string `json:"GoMod"`
+		GoVersion string `json:"GoVersion"`
+	} `json:"Module"`
+}
+
+var (
+	_goList     GoList
+	_goListOnce sync.Once
+)
+
+func GetGoList() GoList {
+	_goListOnce.Do(func() {
+		output, err := exec.Command("go", "list", "-json").Output()
 		if err == nil {
-			return res.Root
+			_ = json.Unmarshal(output, &_goList)
 		}
+	})
+	return _goList
+}
+
+func GoListRoot() string {
+	if strings.HasPrefix(GetGoList().Module.Path, "github.com/starudream/go-lib") && strings.HasSuffix(GetGoList().Module.Path, "/v2") {
+		return filepath.Join(GetGoList().Root, "..")
 	}
-	return ""
+	return GetGoList().Root
 }
