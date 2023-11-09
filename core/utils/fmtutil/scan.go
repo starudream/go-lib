b0995fb538f3f039502033e06a302cd8c0f50f2f
sheng.wang <justwangsheng@gmail.com>
2023-11-09 14:35:57 +0800

    feat(utils): add fmt scan helper

---
+++ core/utils/fmtutil/scan.go
@@ -0,0 +1,30 @@
+package fmtutil
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/starudream/go-lib/core/v2/utils/signalutil"
+)
+
+func Scan(hint string) (s string) {
+	done := make(chan struct{})
+
+	go func() {
+		in := bufio.NewScanner(os.Stdin)
+		for fmt.Print(hint); in.Scan(); fmt.Print(hint) {
+			if s = in.Text(); s != "" {
+				close(done)
+				break
+			}
+		}
+	}()
+
+	select {
+	case <-signalutil.Done():
+	case <-done:
+	}
+
+	return
+}
