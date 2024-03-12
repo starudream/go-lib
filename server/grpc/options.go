e6e42681ded23181eb13bc23b501ae18dff9b220
sheng.wang <justwangsheng@gmail.com>
2024-03-12 14:36:13 +0800

    feat(server): add server options

--- server/grpc/options.go
+++ server/grpc/options.go
@@ -1,7 +1,15 @@
 package grpc
 
 import (
+	"google.golang.org/grpc"
+
 	"github.com/starudream/go-lib/core/v2/utils/optionutil"
 )
 
 type Option = optionutil.I[Server]
+
+func WithServerOptions(opts ...grpc.ServerOption) Option {
+	return optionutil.New(func(s *Server) {
+		s.srvOpts = append(s.srvOpts, opts...)
+	})
+}
