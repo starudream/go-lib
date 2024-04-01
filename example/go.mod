4756f53a3002bd09d0b8db3a14444cd58f321a3a
sheng.wang <justwangsheng@gmail.com>
2024-04-01 11:49:28 +0800

    example

--- example/go.mod
+++ example/go.mod
@@ -21,7 +21,7 @@ replace (
 
 require (
 	github.com/starudream/go-lib/cobra/v2 v2.0.7
-	github.com/starudream/go-lib/core/v2 v2.0.21
+	github.com/starudream/go-lib/core/v2 v2.0.22
 	github.com/starudream/go-lib/cron/v2 v2.0.7
 	github.com/starudream/go-lib/ntfy/v2 v2.0.10
 	github.com/starudream/go-lib/resty/v2 v2.0.10
--- example/service/svc.go
+++ example/service/svc.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 
 	"github.com/starudream/go-lib/core/v2/config/version"
+	"github.com/starudream/go-lib/core/v2/gh"
 	"github.com/starudream/go-lib/core/v2/slog"
 	"github.com/starudream/go-lib/server/v2/grpc"
 	"github.com/starudream/go-lib/server/v2/hggw"
 	"github.com/starudream/go-lib/server/v2/http"
-	"github.com/starudream/go-lib/server/v2/http/middlewares/cors"
+	"github.com/starudream/go-lib/server/v2/http/middlewares"
 	"github.com/starudream/go-lib/server/v2/ictx"
 	"github.com/starudream/go-lib/server/v2/ierr"
+	"github.com/starudream/go-lib/server/v2/jwt"
 
 	"github.com/starudream/go-lib/example/v2/api/admin"
 	"github.com/starudream/go-lib/example/v2/api/admin/user"
@@ -20,10 +22,33 @@ import (
 
 func NewHTTPServer() *hggw.Server {
 	hs := hggw.NewServer()
-	hs.Use(cors.AllowAll())
-	hs.Get("/admin/user/add", func(c *http.Context) error { return c.JSON(200, version.GetVersionInfo()) })
 	hs.RegisterHandler(admin.RegisterAdminUserServiceHandler)
+	RegisterHTTPRouter(hs.With(middlewares.CORS(), middlewares.Logger()))
 	return hs
+}
+
+func RegisterHTTPRouter(r http.Router) {
+	r.HandleCtx("GET /token", func(c *http.Context) error {
+		token, err := jwt.New("*", "starudream", "*").Sign()
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, gh.M{"token": token})
+	})
+	r.HandleCtx("GET /panic", func(c *http.Context) error {
+		panic("panic")
+	})
+	r.HandleCtx("GET /logger", func(c *http.Context) error {
+		if c.GetQuery("error").Bool() {
+			return ierr.BadRequest(9, "request error")
+		}
+		return c.JSON(200, gh.M{"foo": "bar"})
+	})
+	r.With(middlewares.JWT()).HandleCtx("GET /admin/user/add", func(c *http.Context) error {
+		jc := jwt.MustFromContext(c)
+		slog.Info("subject: %s", jc.SUB(), slog.GetAttrs(c))
+		return c.JSON(200, "ok")
+	})
 }
 
 func NewGRPCServer() *grpc.Server {
@@ -44,7 +69,8 @@ func (s *AdminUserService) Health(ctx context.Context, _ *common.Empty) (*common
 }
 
 func (s *AdminUserService) AddUser(ctx context.Context, req *user.AddUserReq) (*common.Id, error) {
-	slog.Info("password: %s", req.Password, slog.GetAttrs(ctx))
+	jc := jwt.MustFromContext(ctx)
+	slog.Info("password: %s, subject: %s", req.Password, jc.SUB(), slog.GetAttrs(ctx))
 	return nil, ierr.Forbidden(1, "no permission")
 }
 
