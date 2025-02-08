8dd5050c33998b0a20248a08559cf15b6072fcac
sheng.wang <justwangsheng@gmail.com>
2025-02-08 22:02:51 +0800

    chore: example

--- example/proto/gen.sh
+++ example/proto/gen.sh
@@ -18,18 +18,18 @@ module github.com/starudream/go-lib/example/v2/api
 go 1.22.0
 
 require (
-	github.com/envoyproxy/protoc-gen-validate v1.1.0
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20241021214115-324edc3d5d38
-	google.golang.org/grpc v1.67.1
-	google.golang.org/protobuf v1.35.1
+	github.com/envoyproxy/protoc-gen-validate v1.2.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.1
+	google.golang.org/genproto/googleapis/api v0.0.0-20250207221924-e9438ea467c6
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250207221924-e9438ea467c6
+	google.golang.org/grpc v1.70.0
+	google.golang.org/protobuf v1.36.5
 )
 
 require (
-	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241021214115-324edc3d5d38 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
 EOF
 go mod tidy
